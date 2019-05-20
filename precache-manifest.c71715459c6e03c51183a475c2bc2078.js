d110aaae5c9b7accebfe253c861a921bfcd58cbc
Andriusjok <andriusjokubauskass@gmail.com>
2019-05-20 10:26:20 +0300

    xd

---
+++ precache-manifest.c71715459c6e03c51183a475c2bc2078.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4d8ef2cb7791815737b",
+    "url": "/build/static/js/runtime~main.9df7376b.js"
+  },
+  {
+    "revision": "bb0dfbffc2133436c3ee",
+    "url": "/build/static/js/main.52aa63c0.chunk.js"
+  },
+  {
+    "revision": "295b214afdb69ba6091a",
+    "url": "/build/static/js/2.baa6f510.chunk.js"
+  },
+  {
+    "revision": "bb0dfbffc2133436c3ee",
+    "url": "/build/static/css/main.6821d17b.chunk.css"
+  },
+  {
+    "revision": "1c4ed8768015c98fa34a7ed216c20360",
+    "url": "/build/index.html"
+  }
+];
